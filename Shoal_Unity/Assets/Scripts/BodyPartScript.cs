d00e23f1270708d9e0c9d98904f8e6fff7ae3b44
Titouan Millet <titouan.millet@gmail.com>
2015-08-04 11:44:51 -0400

    Gradient on fish

--- Shoal_Unity/Assets/Scripts/BodyPartScript.cs
+++ Shoal_Unity/Assets/Scripts/BodyPartScript.cs
@@ -9,13 +9,20 @@ public class BodyPartScript : MonoBehaviour {
 	Color color2B;
 	float transitionVal;
 
+	Material myMat;
+
+	void Awake()
+	{
+		myMat = GetComponent<Renderer>().material;
+	}
+
 
 	public void SetColors(Color mainColor1A, Color mainColor1B, Color mainColor2A, Color mainColor2B)
 	{
-		GetComponent<Renderer>().material.SetColor("_MainColor1A",mainColor1A);
-		GetComponent<Renderer>().material.SetColor("_MainColor1B",mainColor1B);
-		GetComponent<Renderer>().material.SetColor("_MainColor2A",mainColor2A);
-		GetComponent<Renderer>().material.SetColor("_MainColor2B",mainColor2B);
+		myMat.SetColor("_MainColor1A",mainColor1A);
+		myMat.SetColor("_MainColor1B",mainColor1B);
+		myMat.SetColor("_MainColor2A",mainColor2A);
+		myMat.SetColor("_MainColor2B",mainColor2B);
 
 		color1A = mainColor1A;
 		color1B = mainColor1B;
@@ -25,50 +32,55 @@ public class BodyPartScript : MonoBehaviour {
 
 	public void SetLerpsColors(float lerpMainColor1,float lerpMainColor2)
 	{
-		GetComponent<Renderer>().material.SetFloat("_LerpColor1",lerpMainColor1);
-		GetComponent<Renderer>().material.SetFloat("_LerpColor2",lerpMainColor2);
+		myMat.SetFloat("_LerpColor1",lerpMainColor1);
+		myMat.SetFloat("_LerpColor2",lerpMainColor2);
 	}
 
 	public void SetTransitionColorValue(float transitionValue)
 	{
-		GetComponent<Renderer>().material.SetFloat("_TransitionValue",transitionValue);
+		myMat.SetFloat("_TransitionValue",transitionValue);
 		transitionVal = transitionValue;
 	}
 
 	public void SetOutlineWidth(float outlineWidth)
 	{
-		GetComponent<Renderer>().material.SetFloat("_OutlineWidth",outlineWidth);
+		myMat.SetFloat("_OutlineWidth",outlineWidth);
 	}
 
 	public Color[] GetColors()
 	{
 		Color[] colors = new Color[2];
-		colors[0] = Color.Lerp(GetComponent<Renderer>().material.GetColor("_MainColor1A"),GetComponent<Renderer>().material.GetColor("_MainColor1B"),GetComponent<Renderer>().material.GetFloat("_LerpColor1"));
-		colors[1] = Color.Lerp(GetComponent<Renderer>().material.GetColor("_MainColor2A"),GetComponent<Renderer>().material.GetColor("_MainColor2B"),GetComponent<Renderer>().material.GetFloat("_LerpColor2"));
+		colors[0] = Color.Lerp(myMat.GetColor("_MainColor1A"),myMat.GetColor("_MainColor1B"),myMat.GetFloat("_LerpColor1"));
+		colors[1] = Color.Lerp(myMat.GetColor("_MainColor2A"),myMat.GetColor("_MainColor2B"),myMat.GetFloat("_LerpColor2"));
 		return(colors);
 	}
 
 	public Color GetTransitionColor()
 	{
-		Color col1 = Color.Lerp (color1A,color1B,GetComponent<Renderer>().material.GetFloat("_LerpColor1"));
-		Color col2 = Color.Lerp (color2A,color2B,GetComponent<Renderer>().material.GetFloat("_LerpColor2"));
+		Color col1 = Color.Lerp (color1A,color1B,myMat.GetFloat("_LerpColor1"));
+		Color col2 = Color.Lerp (color2A,color2B,myMat.GetFloat("_LerpColor2"));
 
-		Color colI = Color.Lerp (col1,col2,GetComponent<Renderer>().material.GetFloat("_TransitionValue"));
-		Color colJ = Color.Lerp (col2,col1,GetComponent<Renderer>().material.GetFloat("_TransitionValue"));
+		Color colI = Color.Lerp (col1,col2,myMat.GetFloat("_TransitionValue"));
+		Color colJ = Color.Lerp (col2,col1,myMat.GetFloat("_TransitionValue"));
 
-		return(Color.Lerp (colI,colJ,1f-GetComponent<Renderer>().material.GetFloat("_OutlineWidth")));
+		return(Color.Lerp (colI,colJ,1f-myMat.GetFloat("_OutlineWidth")));
 
 
+	}
+
+	public void SetCustomColor(Color value)
+	{
+		myMat.SetColor ("_myCustomColor",value);
 	}
 
 	public void SetSize(float size)
 	{
-		GetComponent<Renderer>().material.SetFloat("_Size",size);
+		myMat.SetFloat("_Size",size);
 	}
 
 	public float GetSize()
 	{
-		return(GetComponent<Renderer>().material.GetFloat("_Size"));
+		return(myMat.GetFloat("_Size"));
 	}
 
 

58643653e272c2ccff9f0fe2012431c66ea2bb55
Tobias Wehrum <Tobias.Wehrum@dragonlab.de>
2015-08-11 12:07:42 -0400

    Fixed alignment

--- Shoal_Unity/Assets/Scripts/Behaviour/MovementAlignment.cs
+++ Shoal_Unity/Assets/Scripts/Behaviour/MovementAlignment.cs
@@ -6,6 +6,6 @@ public class MovementAlignment : MovementFlockingBase
 {
     protected override Vector2 GetEntityForce(Entity otherEntity, Vector2 delta, float closenessPercent)
     {
-        return -otherEntity.GetComponent<Vehicle>().Velocity * closenessPercent;
+        return otherEntity.GetComponent<Vehicle>().Velocity * closenessPercent;
     }
 }

7dd57d046f5ab756f63193938244a01b28113596
Tobias Wehrum <Tobias.Wehrum@dragonlab.de>
2015-08-17 17:57:47 -0400

    Sfx Manager

---
+++ Shoal_Unity/Assets/Scripts/Audio/AudioClipWithVolume.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class AudioClipWithVolume
+{
+    [SerializeField] private bool enabled = true;
+    [SerializeField] private AudioClip clip;
+    [SerializeField] private float volume = 1f;
+
+    public bool Enabled { get { return enabled; } }
+    public AudioClip Clip { get { return clip; } }
+    public float Volume { get { return volume; } }
+}
